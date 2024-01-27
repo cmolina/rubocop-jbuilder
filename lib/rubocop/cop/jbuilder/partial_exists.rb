7d986a8af3460dcb3467a426471901529fb73512
Carlos Molina A <hi@cmolina.dev>
2024-01-27 11:42:57 -0300

    PartialExists gets support to `json.array!`
    - Rename node matcher to `has_partial?`, and use `{ }` for matching any of the listed patterns
    - Split the unit tests in context groups

--- lib/rubocop/cop/jbuilder/partial_exists.rb
+++ lib/rubocop/cop/jbuilder/partial_exists.rb
@@ -8,14 +8,17 @@ module RuboCop
       class PartialExists < Base
         MSG = "Partial not found. Looked for: %<path>s"
 
-        RESTRICT_ON_SEND = %i[partial!].freeze
+        RESTRICT_ON_SEND = %i[partial! array!].freeze
 
-        def_node_matcher :partial_method?, <<~PATTERN
-          (send (send nil? :json) :partial! $str ...)
+        def_node_matcher :has_partial?, <<~PATTERN
+          {
+            (send (send nil? :json) :partial! $str ...)
+            (send (send nil? :json) :array! ... (hash (pair (sym :partial) $str) ...))
+          }
         PATTERN
 
         def on_send(node)
-          partial_method?(node) do |actual|
+          has_partial?(node) do |actual|
             full_path = get_view_path_from(actual)
             path = full_path.sub(rails_root_dirname(node) + "/", "")
             add_offense(actual, message: format(MSG, path:)) unless File.exist?(full_path)
--- spec/rubocop/cop/jbuilder/partial_exists_spec.rb
+++ spec/rubocop/cop/jbuilder/partial_exists_spec.rb
@@ -3,23 +3,47 @@
 RSpec.describe RuboCop::Cop::Jbuilder::PartialExists, :config do
   let(:config) { RuboCop::Config.new }
 
-  it 'for "json.partial!" register an offense when partial does not exist' do
-    expect_offense(<<~RUBY)
-      json.partial! 'comments/comment', comments: @message.comment
-                    ^^^^^^^^^^^^^^^^^^ Jbuilder/PartialExists: Partial not found. Looked for: app/views/comments/_comment.json.jbuilder
-    RUBY
+  context 'with "json.partial!"' do
+    it "register an offense when partial does not exist" do
+      expect_offense(<<~RUBY)
+        json.partial! 'comments/comment', comments: @message.comment
+                      ^^^^^^^^^^^^^^^^^^ Jbuilder/PartialExists: Partial not found. Looked for: app/views/comments/_comment.json.jbuilder
+      RUBY
+    end
+
+    it "register an offense when local partial does not exist" do
+      expect_offense(<<~RUBY)
+        json.partial! 'comment', comments: @message.comment
+                      ^^^^^^^^^ Jbuilder/PartialExists: Partial not found. Looked for: app/views/testing/_comment.json.jbuilder
+      RUBY
+    end
+
+    it "does not register an offense when partial exists" do
+      expect_no_offenses(<<~RUBY)
+        json.partial! 'exists'
+      RUBY
+    end
   end
 
-  it 'for "json.partial!" register an offense when local partial does not exist' do
-    expect_offense(<<~RUBY)
-      json.partial! 'comment', comments: @message.comment
-                    ^^^^^^^^^ Jbuilder/PartialExists: Partial not found. Looked for: app/views/testing/_comment.json.jbuilder
-    RUBY
-  end
+  context 'with "json.array!"' do
+    it "register an offense when partial does not exist" do
+      expect_offense(<<~RUBY)
+        json.array! @comments, partial: 'comments/comment', as: :comment
+                                        ^^^^^^^^^^^^^^^^^^ Jbuilder/PartialExists: Partial not found. Looked for: app/views/comments/_comment.json.jbuilder
+      RUBY
+    end
 
-  it "does not register an offense when partial exists" do
-    expect_no_offenses(<<~RUBY)
-      json.partial! 'exists'
-    RUBY
+    it "register an offense when local partial does not exist" do
+      expect_offense(<<~RUBY)
+        json.array! @comments, partial: 'comment', as: :comment
+                                        ^^^^^^^^^ Jbuilder/PartialExists: Partial not found. Looked for: app/views/testing/_comment.json.jbuilder
+      RUBY
+    end
+
+    it "does not register an offense when partial exists" do
+      expect_no_offenses(<<~RUBY)
+        json.array! @exists, partial: 'exists', as: :exists
+      RUBY
+    end
   end
 end
